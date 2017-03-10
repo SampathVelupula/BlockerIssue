e199f8147cd60bae7e58d7bca397f97cb4e762bd
Sampath Velupula <sampath.velupula@teamaol.com>
2017-03-10 14:16:08 -0800

    test

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,10 @@
+var fs = require('fs');
+var moment = require('moment');
+
+var timestamp = moment().unix();
+
+var wstream = fs.createWriteStream('browser-log-' + timestamp + '.txt');
+
 module.exports = {
     'Demo test' : function (browser) {
 
@@ -6,7 +13,14 @@ module.exports = {
             .waitForElementVisible('#control', 2000)
             .click('#control')
             .pause(40000)
-            .end();
+            .url('http://google.com')
+            .getLog('browser', function(logEntriesArray) {
+                console.log('Log length: ' + logEntriesArray.length);
+                logEntriesArray.forEach(function(log) {
+                    console.log('[' + log.level + '] ' + log.timestamp + ' : ' + log.message);
+                    wstream.write('[' + log.level + '] ' + log.timestamp + ' : ' + log.message + '\n\n');
+                });
+            });
         browser.expect.element('#main').to.be.visible;
 
     }
